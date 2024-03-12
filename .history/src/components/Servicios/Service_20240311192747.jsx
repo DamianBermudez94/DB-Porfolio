fd8ff9cda5176d4ecaa492876a72f5f5c872e86f
Damian Bermudez <bermudezdamian7@gmail.com>
2024-03-11 23:34:35 -0300

    se cambio el layout del componente servicios

---
+++ .history/src/components/Servicios/Service_20240311192747.jsx
@@ -0,0 +1,56 @@
+
+import { NavLink } from "react-router-dom";
+import "./styles.css"
+export const Service = () => {
+  return (
+    <section
+    className="section-servicios animacionIzquierda"
+    id="mis-servicios"
+  >
+    <h3 className="title-servicios">Mis Servicios</h3>
+    <div className="container-card_servicios">
+      <article className="card">
+        <div className="card-details">
+          <p className="text-title">Web Design</p>
+          <p className="text-body">
+            {" "}
+            Mi trabajo se basa en el desarrollo de las interfaces de usuario
+            para Singles Pages Apps montadas en la web, utilizando
+            diferentes hostings para poder alojarlas. Donde tambíen consumo
+            API´s REST para poder montar el contenido. Utilizando el
+            paradigma de programación reactiva por medio de un estado y
+            routers para mantener actualizados los entornos y redirigir la
+            navegación. Lo que permite que el mismo entorno sea suceptible y
+            escuche los cambios que vengan desde diferentes puntos de de la
+            App.
+          </p>
+        </div>
+        <NavLink className="card-button" to="/contacto">
+          Más info
+        </NavLink>
+      </article>
+      <article className="card">
+        <div className="card-details">
+          <p className="text-title">BackEnd</p>
+          <p className="text-body">
+            {" "}
+            <p>
+              Como Desarrollador FullStack, creo mis propias API´s REST para
+              que puedan ser consumidas facilmente por un FrontEnd utilizando
+              el esquema MVC (Model/View/Controller). Donde utilizo Postman
+              para diseñar y los endpoints, Node.Js con Express para el
+              montado y brindando la capacidad de alojar la data consumida
+              tanto en una base de datos SQL como No-SQL. Sumandole el uso
+              de algunas bases de datos extras como Algolia para poder
+              efectuar busquedas complejas.
+            </p>
+          </p>
+        </div>
+        <NavLink className="card-button" to="/contacto">
+          Más info
+        </NavLink>
+      </article>
+    </div>
+  </section>
+  )
+}
