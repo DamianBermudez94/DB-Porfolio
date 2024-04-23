46b975d081b7b181cadfe7debf3d723ae889520f
Damian Bermudez <bermudezdamian7@gmail.com>
2024-04-22 22:22:05 -0300

    Se cambio logo

---
+++ .history/src/components/Home/Home_20240420181614.jsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import LazyLoad from "react-lazyload";
+import { NavLink } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
+import imagen from "../../../public/image/Developer.png";
+import { Service } from "../Servicios/Service";
+import ButtonFlotante from "../ButtonFlotante/ButtonFlotante";
+import { ButtonInformacion } from "../ButtonInformacion/ButtonInformacion";
+import "./styles.css";
+
+export const Home = () => {
+  const [showContent, setShowContent] = useState(false);
+  // Utilizamos useEffect para activar la animación cuando el componente se monta
+  useEffect(() => {
+    setShowContent(true);
+  }, []);
+
+  // Solo se ejecuta una vez cuando el componente se monta
+
+  // Definimos la animación usando useSpring
+  const contentAnimation = useSpring({
+    opacity: showContent ? 1 : 0, // Mostrar contenido cuando showContent sea true
+    transform: showContent ? "translateY(0px)" : "translateY(-100px)", // Animación de desplazamiento hacia arriba
+    from: { opacity: 0, transform: "translateY(-100px)" }, // Estilo inicial
+    delay: 500, // Retraso de la animación
+  });
+  return (
+    <>
+      <section className="home">
+        <animated.div className="home-welcome" style={contentAnimation}>
+          <div className="container-home">
+            <div className="content">
+              <div className="content-title__principal">
+                <strong>¡Bienvenidos!</strong>
+                <b>
+                  <h1 className="innerIam">
+                    Me llamo
+                    <br />
+                    Damián Bermúdez
+                    <br />
+                    Developer Web Full Stack,
+                    <br />
+                    Diseñador Web
+                    <br />
+                  </h1>
+                </b>
+              </div>
+              <div className="container-redes_inicio">
+                <NavLink
+                  to="https://www.linkedin.com/in/damian-bermudez-4a4a33204/"
+                  className="link-redes_sociales"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin linkedin"></i>
+                </NavLink>
+                <NavLink
+                  to="https://github.com/DamianBermudez94"
+                  className="link-redes_sociales"
+                  target="_blank"
+                >
+                  <i className="fab fa-github-square github"></i>
+                </NavLink>
+                <a
+                  className="btn-cv"
+                  href="./cv-pdf/CVDamianBermudez2023_1_ (1).pdf"
+                  download="CV-damian-bermudez.pdf"
+                >
+                  CV
+                </a>
+              </div>
+            </div>
+            <LazyLoad className="lazy-load" height={200} offset={100}>
+              <img
+                src={"/image/profile-pic.png"}
+                alt="Damian Bermudez desarrollador web"
+              ></img>
+            </LazyLoad>
+          </div>
+          <ButtonFlotante />
+        </animated.div>
+      </section>
+      <section className="section-me" style={contentAnimation}>
+     
+        <div className="content-title">
+          <h2 className="title-sobre__me">Sobre mi</h2>
+          <span className="sub-title">Sobre mi</span>
+        </div>
+        <animated.div
+          className="container-me"
+          data-animation="diagonal"
+          style={contentAnimation}
+        >
+          <article>
+            <LazyLoad height={200} offset={100}>
+              <img
+                src={imagen}
+                alt="Developer Web Full Stack - Desarrollador Web Full stack - Paginas Web"
+              />
+            </LazyLoad>
+            <p>
+              ¡Hola! Soy un apasionado{" "}
+              <strong>Desarrollador Web Full Stack y Diseñador Web</strong> en
+              búsqueda de nuevas oportunidades para seguir expandiendo mis
+              horizontes profesionales. Con más de dos años y medio dedicados a
+              perfeccionarme en las tecnologías más demandadas del mercado,
+              estoy listo para ofrecer soluciones innovadoras y de alta calidad
+              para tus proyectos. Mi portafolio incluye proyectos web de diversa
+              envergadura. Desde la creación de sitios web responsive hasta la
+              optimización del rendimiento y la implementación de
+              características interactivas, me enorgullezco de resolver
+              problemas técnicos de manera efectiva
+            </p>
+          </article>
+          <ButtonInformacion text="¡Demos vida a tu idea!" />
+        </animated.div>
+      </section>
+      <section>
+        <Service />
+      </section>
+    </>
+  );
+};
---
+++ .history/src/data/trabajos_20240420142348.js
@@ -0,0 +1,92 @@
+export const trabajos = [
+  {
+    id: "E-commerce-proyecto",
+    nombre: "E-commerce",
+    url: "https://desafio-final-mod10.vercel.app/",
+    git: "https://github.com/DamianBermudez94/desafio-final-mod10",
+    tipo:"curso",
+    tecnologias:
+      "React- Netx Js - TypeScript - JavaScript - Styled_components - Swr - Recoil - React-Spring - React-Hook-Form",
+    categorias: "Web app",
+    description:
+      "E-Commerce desarrollado en Next y React donde los usuarios pueden buscar y comprar muebles uilizando MercadoPago como pasarela de pago. (El cobro solo funciona con cuentas de prueba) Se utilizó Styled-components para desarrollar la UI, SWR para fetchear la data y Recoil como manejador de estado. Se aplicaron técnicas como: Server Static Generation (SSG) y Critical Css para mejorar el rendimiento. El login se realiza mediante autorización passwordless. Posee excelentes métricas en auditoría de LightHouse.",
+  },
+  {
+    id: "proyecto-mercedes-michelini",
+    nombre: "Mercedes Michelini",
+    url: "http://www.michelinimercedes.com/",
+    git: "https://github.com/DamianBermudez94/mercedes-michelini",
+    tipo:"freelance",
+    tecnologias: "HTML - CSS - JavaScript - CMS",
+    categorias: "Página web",
+    description:
+      "Sitio web para negocio inmobiliario: Diseño 100% personalizado, estilo 100% adaptable a diferentes dispositivos. Muestra infomación acerca del producto/servicio que ofrece, cuenta con 4 secciones,SEO, Optimización de carga, link a redes sociales.",
+  },
+  {
+    id: "landing-page-sp-seguridad",
+    nombre: "Sp Seguridad",
+    url: "https://damianbermudez94.github.io/sp-seguridad/",
+    git: "https://github.com/DamianBermudez94/sp-seguridad",
+    tipo:"freelance",
+    tecnologias: "HTML - CSS - JavaScript",
+    categorias: "Página web",
+    description:
+      "Página de aterrizaje para negocio de camaras de seguridad: Diseño 100% personalizado, estilo 100% adaptable a diferentes dispositivos. Muestra infomación acerca del producto/servicio que ofrece, formulario de contacto, link redes sociales.",
+  },
+  {
+    id: "porfolio-proyecto",
+    nombre: "DB|WEB",
+    url: "https://www.damianbermudezdev.es/",
+    git: "https://github.com/DamianBermudez94/",
+    tipo:"freelance",
+    tecnologias: "React JS - JavaScript - CSS - HTML",
+    categorias: "Porfolio web",
+    description:
+      "Porfolio personalizado donde se implemento:Diseño:moderno y 100% personalizado y adaptable a todos los dispositivos. Componentización con React: para aprovechar el poder de la componentización de React para crear un código limpio, modular y fácil de mantener. Optimización de Rendimiento: Se utilizaron tecnicas de optimizacion para la carga de imagenes, minificacion de archivos y el almacenamiento en cache del navegador, para garantizar tiempos de carga rápidos y una navegación eficiente",
+  },
+  {
+    id: "Perfil-plac-proyecto",
+    nombre: "Perfil-plac",
+    url: "https://perfil-plac.com/",
+    git: "https://github.com/DamianBermudez94/",
+    tipo:"freelance",
+    tecnologias: "React JS - JavaScript - CSS - HTML",
+    categorias: "Sitio Web",
+    description:
+      "Sitio web de venta y colocación para la construcción en seco :Diseño moderno, 100% personalizado y adaptable a todos los dispositivos. Se utilizó React para aprovechar el poder de la componentización para crear un código limpio, modular y fácil de mantener. Optimización de Rendimiento: Se utilizaron técnicas de optimización para la carga de imágenes, minificación de archivos y el almacenamiento en caché del navegador, para garantizar tiempos de carga rápidos y una navegación eficiente. SEO: Se utilizaron técnicas para la optimización de los robots de búsquedas para garantizar buena posición en las búsquedas.",
+  }
+  ,
+  {
+    id: "red-social-proyecto",
+    nombre: "clon-red-social",
+    url: "https://frond-end-red-social.vercel.app/",
+    git: "https://github.com/DamianBermudez94/",
+    tipo:"curso",
+    tecnologias: "React js - Node js - Express - MongoDB",
+    categorias: "Web App",
+    description:
+      "Clon de Red Social: creación API con Express. Se implemento patrón MVC ( MODELO - VISTA - CONTROLADOR ), JWT para la autenticación del usuario,Mongo DB como base de datos para manejar la información de la API. Para el front endSe utilizo React Js como principal tecnología para el FRONT-ENDReact-router, hooks, context",
+  },
+  {
+    id: "piedrapapeltijera",
+    nombre: "Piedra papel o tijeras",
+    url: "https://dwf-m7-mod-final.herokuapp.com/",
+    git: "",
+    tipo:"curso",
+    tecnologias: "HTML - CSS - JavaScript",
+    categorias: "Web app",
+    description:
+      "Aplicación web donde permite jugar el clasico juego en dos opciones, contra la maquina y de forma online ",
+  },
+  {
+    id: "buscadordemascotas",
+    nombre: "Buscador de mascotas",
+    url: "https://damianbermudezdev.es/",
+    git: "https://github.com/DamianBermudez94/dwf-m8-desafio-final",
+    tipo:"curso",
+    tecnologias: "React - Typescript - Webpack - Recoil - Mapbox - Sendgrid",
+    categorias: "Web app",
+    description:
+      "Webapp donde podrás reportar mascotas perdidas. Se utilizaron patrones MVC y REST, una base de datos en PostreSQL. Posee un sistema de autenticación y utiliza Algolia y Mapbox para integrar geolocalización en el sistema.",
+  }
+];
