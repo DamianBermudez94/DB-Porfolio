fa4ce1988a7727defceb253e7c01ba563984ebc6
Damian Bermudez <bermudezdamian7@gmail.com>
2024-04-25 23:07:04 -0300

    se cambio diseño de la home principal

---
+++ .history/src/App_20240425182846.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import LazyLoad from 'react-lazyload';
+import { MisRutas } from "./routers/MisRutas";
+import { BrowserRouter } from "react-router-dom";
+import backgroundImage from "../public/image/banner-parallax2.jpg";
+import "./App.css";
+function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.pageYOffset);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <main className="content-main parallax-container">
+      <LazyLoad
+        className="parallax-background"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPositionY: scrollPosition / 2 + "px",
+        }}
+      />
+      <div className="parallax-content">
+        <BrowserRouter>
+          <MisRutas />
+        </BrowserRouter>
+      </div>
+    </main>
+  );
+}
+
+export default App;
