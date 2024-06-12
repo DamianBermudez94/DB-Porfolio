6ceb1b5b5ddeac9d916051bd65426d1cadf61e41
Damian Bermudez <bermudezdamian7@gmail.com>
2024-06-11 22:51:23 -0300

    se termino de ajustar retoques en versión responisve en los diferentes componentes

---
+++ .history/src/components/Contacto/Contact_20240611185001.jsx
@@ -0,0 +1,82 @@
+import "./styles.css";
+export const Contact = () => {
+  return (
+    <section className="container-form" data-animation="diagonal" id="contacto">
+      <div className="content">
+        <div>
+          <h1 className="title-contacto">Contacto</h1>
+          <span className="sub-title__contacto">Contacto</span>
+        </div>
+        <div className="content-redes__sociales">
+          <a
+            href="https://www.facebook.com"
+            className="link-redes_sociales"
+            target="_blank"
+          >
+            <i className="fab fa-facebook facebook"></i>
+          </a>
+          <a
+            href="https://www.instagram.com"
+            className="link-redes_sociales"
+            target="_blank"
+          >
+            <i className="fab fa-instagram instagran"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com"
+            className="link-redes_sociales"
+            target="_blank"
+          >
+            <i className="fab fa-linkedin linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/DamianBermudez94"
+            className="link-redes_sociales"
+            target="_blank"
+          >
+            <i className="fab fa-github-square github"></i>
+          </a>
+        </div>
+      </div>
+
+      <form id="formulario">
+        <input
+          type="text"
+          id="nombre"
+          name="nombre"
+          placeholder="Escriba su nombre..."
+          required
+        />
+        <input
+          type="tel"
+          id="telefono"
+          name="telefono"
+          placeholder="Escriba su numero de telefono..."
+          required
+        />
+        <input
+          type="email"
+          id="email"
+          name="correo"
+          placeholder="Escriba su email..."
+          required
+        />
+        <textarea
+          name="asunto"
+          id="mensaje"
+          placeholder="Deje su mensaje......"
+          required
+        ></textarea>
+        <button type="submit" className="btn-enviar">
+          <span>Enviar</span>
+          <span></span>
+        </button>
+        <div className="mensaje-form">
+          <strong>
+            * No dudes en consultarme, te estare respondiendo a la brevedad *
+          </strong>
+        </div>
+      </form>
+    </section>
+  );
+};
